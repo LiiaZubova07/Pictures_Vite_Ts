9c8325089c2b36540cccb47c5a2ed57ce2b4a715
Liia Zubova <vardlitmo@gmail.com>
2023-01-09 20:03:59 +0300

    add: sliders.js

---
+++ src/js/modules/sliders.js
@@ -0,0 +1,16 @@
+//значение в виде строки, селекторы отвечающие за кнопки
+const sliders = (slides, dir, prev, next) => {
+  //показывается первый слайд на странице
+  const slideIndex = 1;
+  //создание элементов
+  const items = document.querySelectorAll(slides);
+  const prevBtn = document.querySelectorAll(prev);
+  const nextBtn = document.querySelectorAll(next);
+
+  //функция, перемещающая slideIndex
+  const showSlides = (n) => {
+	
+  };
+};
+
+export default sliders;
