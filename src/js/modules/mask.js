19e425d07a441444497e5cdc21034c4ecbd1b677
Liia Zubova <vardlitmo@gmail.com>
2023-01-13 20:13:30 +0300

    add: phone mask

--- src/js/modules/mask.js
+++ src/js/modules/mask.js
@@ -11,16 +11,16 @@ const mask = (selector) => {
       range.collapse(true);
       range.moveEnd('character', pos);
       range.moveStart('character', pos);
-		range.select();
+      range.select();
     }
   };
 
-  const createMask = (event) => {
+  function createMask(event) {
     const matrix = '+7 (___) ___ __ __';
-    const i = 0;
+    let i = 0;
     //получать все НЕ цифры, которые там есть
-    const def = matrix.replace(/\D/g, '');
-    const val = this.value.replace(/\D/g, '');
+    let def = matrix.replace(/\D/g, '');
+    let val = this.value.replace(/\D/g, '');
 
     if (def.length >= val.length) {
       val = def;
@@ -37,15 +37,15 @@ const mask = (selector) => {
     } else {
       setCursorPosition(this.value.length, this);
     }
-  };
+  }
 
   const inputs = document.querySelectorAll(selector);
 
-  inputs.forEach(input =>{
-input.addEventListener('input',createMask);
-input.addEventListener('focus',createMask);
-input.addEventListener('blur',createMask);
-  })
+  inputs.forEach((input) => {
+    input.addEventListener('input', createMask);
+    input.addEventListener('focus', createMask);
+    input.addEventListener('blur', createMask);
+  });
 };
 
 export default mask;
