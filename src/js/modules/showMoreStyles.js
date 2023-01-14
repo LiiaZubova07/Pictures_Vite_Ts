cac2f2772cdc87b78ca162aba8f85cef39f7dad0
Liia Zubova <vardlitmo@gmail.com>
2023-01-14 19:12:26 +0300

    add: db.json

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -3,17 +3,17 @@ const showMoreStyles = (trigger, styles) => {
   const btn = document.querySelector(trigger);
 
   //анимации
-  cards.forEach((card) => {
-    card.classList.add('animated', 'fadeInUp');
-  });
+//   cards.forEach((card) => {
+//     card.classList.add('animated', 'fadeInUp');
+//   });
   
-  btn.addEventListener('click', () => {
-    cards.forEach((card) => {
-      card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
-      card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
-    });
-	//  btn.style.display='none';
-	 btn.remove();
-  });
+//   btn.addEventListener('click', () => {
+//     cards.forEach((card) => {
+//       card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
+//       card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+//     });
+// 	//  btn.style.display='none';
+// 	 btn.remove();
+//   });
 };
 export default showMoreStyles;
