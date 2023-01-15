22c987c7a2f4c4f6864fedcec205b5906b8a5384
Liia Zubova <vardlitmo@gmail.com>
2023-01-15 21:54:16 +0300

    add: btns

---
+++ src/js/modules/filter.js
@@ -0,0 +1,86 @@
+const filter = () => {
+  //добавляем элементы, с которыми будем работать
+  const menu = document.querySelector('.portfolio-menu');
+  const items = menu.querySelectorAll('li');
+  const btnAll = menu.querySelector('.all');
+  const btnLovers = menu.querySelector('.lovers');
+  const btnChef = menu.querySelector('.chef');
+  const btnGirl = menu.querySelector('.girl');
+  const btnGuy = menu.querySelector('.guy');
+  const btnGrandmother = menu.querySelector('.grandmother');
+  const btnGrandfather = menu.querySelector('.grandfather');
+
+  const wrapper = document.querySelector('.portfolio-wrapper');
+
+  const markAll = wrapper.querySelectorAll('.all');
+  const markGirl = wrapper.querySelectorAll('.girl');
+  const markLovers = wrapper.querySelectorAll('.lovers');
+  const markChef = wrapper.querySelectorAll('.chef');
+  const markGuy = wrapper.querySelectorAll('.guy');
+
+  const no = document.querySelector('.portfolio-no');
+
+  //фильтрация элементов
+  //скрыть ненужные элементы и показать нужные
+  const typeFilter = (markType) => {
+    markAll.forEach((mark) => {
+      mark.style.display = 'none';
+      mark.classList.remove('animated', 'fadeIn');
+    });
+
+    no.style.display = 'none';
+    no.classList.remove('animated', 'fadeIn');
+
+    if (markType) {
+      markType.forEach((mark) => {
+        mark.style.display = 'block';
+        mark.classList.add('animated', 'fadeIn');
+      });
+    } else {
+      no.style.display = 'block';
+      no.classList.add('animated', 'fadeIn');
+    }
+
+    //  markType
+    //    ? markType.forEach((mark) => {
+    //        (mark.style.display = 'block'), mark.classList.add('animated', 'fadeIn');
+    //      })
+    //    : ((no.style.display = 'block'), no.classList.add('animated', 'fadeIn'));
+  };
+
+  const btn = (btnName, markName) => {
+    btnName.addEventListener('click', () => {
+      typeFilter(markName);
+    });
+  };
+
+  btn(btnAll, markAll);
+  btn(btnLovers, markLovers);
+
+  //используем кнопки
+  btnAll.addEventListener('click', () => {
+    typeFilter(markAll);
+  });
+  btnLovers.addEventListener('click', () => {
+    typeFilter(markLovers);
+  });
+
+  
+  btnChef.addEventListener('click', () => {
+    typeFilter(markChef);
+  });
+  btnGirl.addEventListener('click', () => {
+    typeFilter(markGirl);
+  });
+  btnGuy.addEventListener('click', () => {
+    typeFilter(markGuy);
+  });
+  btnGrandmother.addEventListener('click', () => {
+    typeFilter();
+  });
+  btnGrandfather.addEventListener('click', () => {
+    typeFilter();
+  });
+};
+
+export default filter;
