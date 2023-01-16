012959843ed315eb0f693730f369c5bfd08a3cc7
Liia Zubova <vardlitmo@gmail.com>
2023-01-16 22:00:35 +0300

    fix: accordion.js syntax

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -7,9 +7,9 @@ const accordion = ({ triggersSelector }) => {
       this.nextElementSibling.classList.toggle('active-content');
 
       if (this.classList.contains('active-style')) {
-        this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + 'px';
+        this.nextElementSibling.style.maxHeight = `${this.nextElementSibling.scrollHeight + 80}px`;
       } else {
-        this.nextElementSibling.style.maxHeight = '0px';
+        this.nextElementSibling.style.maxHeight = '0';
       }
     });
   });
