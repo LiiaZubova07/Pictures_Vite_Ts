c2a7380f372549f15b4e4875cefbae0a72c0d25c
Liia Zubova <vardlitmo@gmail.com>
2023-01-18 17:02:32 +0300

    add: arrow

---
+++ src/js/modules/scrolling.js
@@ -0,0 +1,15 @@
+const scrolling = (upSelector) => {
+  const upElem = document.querySelector(upSelector);
+
+  window.addEventListener('scroll', () => {
+    if (document.documentElement.scrollTop > 1650) {
+      upElem.classList.add('animated', 'fadeIn');
+      upElem.classList.remove('fadeOut');
+    } else {
+      upElem.classList.add('fadeOut');
+      upElem.classList.remove('fadeIn');
+    }
+  });
+};
+
+export default scrolling;
