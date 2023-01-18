7c51c1ccc079e628523cd776f1908f82ca621ee0
Liia Zubova <vardlitmo@gmail.com>
2023-01-18 15:35:35 +0300

    add: BREAKPOINT

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -2,12 +2,14 @@ const burger = ({ menuSelector, burgerSelector }) => {
   const menuElem = document.querySelector(menuSelector);
   const burgerElem = document.querySelector(burgerSelector);
 
+  const BREAKPOINT = 992;
+
   //скрыть меню вручную
   menuElem.style.display = 'none';
 
   //отслеживание действий, которые происходят на бургере
   burgerElem.addEventListener('click', () => {
-    if (menuElem.style.display == 'none' && window.screen.availWidth < 993) {
+    if (menuElem.style.display == 'none' && window.screen.availWidth <= BREAKPOINT) {
       menuElem.style.display = 'block';
     } else {
       menuElem.style.display = 'none';
@@ -16,7 +18,7 @@ const burger = ({ menuSelector, burgerSelector }) => {
 
   //отслеживание изменения размеров пользователем
   window.addEventListener('resize', () => {
-    if (window.screen.availWidth > 992) {
+    if (window.screen.availWidth > BREAKPOINT) {
       menuElem.style.display = 'none';
     }
   });
