5fbee5772a3631a159df79b159d9041e83191d63
Liia Zubova <vardlitmo@gmail.com>
2023-02-08 17:15:41 +0300

    add: drop_check.ts

--- src/js/modules/drop.ts
+++ src/js/modules/drop.ts
@@ -10,57 +10,63 @@ const drop = () => {
 
   // с * срабатывает на элементе, который перетаскиваем, не сработают в нашей задаче
 
-  const fileInputs = document.querySelectorAll('[name="upload"]');
+  const fileInputs = document.querySelectorAll(
+    '[name="upload"]'
+  ) as NodeListOf<HTMLInputElement>;
 
-  const preventDefaults = (e) => {
+  const preventDefaults = (e: Event) => {
     e.preventDefault();
     e.stopPropagation();
   };
 
-  const highlight = (item) => {
-    item.closest('.file_upload').style.border = '5px solid yellow';
-    item.closest('.file_upload').style.backgroundColor = 'rgba(0,0,0, 0.7)';
+  const highlight = (item: HTMLElement) => {
+    (item.closest(".file_upload") as HTMLElement).style.border =
+      "5px solid yellow";
+    (item.closest(".file_upload") as HTMLElement).style.backgroundColor =
+      "rgba(0,0,0, 0.7)";
   };
-  const unhighlight = (item) => {
-    item.closest('.file_upload').style.border = 'none';
-    if (item.closest('.calc_form')) {
-      item.closest('.file_upload').style.backgroundColor = '#fff';
+  const unhighlight = (item: HTMLElement) => {
+    (item.closest(".file_upload") as HTMLElement).style.border = "none";
+    if (item.closest(".calc_form")) {
+      (item.closest(".file_upload") as HTMLElement).style.backgroundColor =
+        "#fff";
     } else {
-      item.closest('.file_upload').style.backgroundColor = '#ededed';
+      (item.closest(".file_upload") as HTMLElement).style.backgroundColor =
+        "#ededed";
     }
   };
 
   //массив событий
-  ['dragenter', 'dragleave', 'dragover', 'drop'].forEach((eventName) => {
+  ["dragenter", "dragleave", "dragover", "drop"].forEach((eventName) => {
     fileInputs.forEach((input) => {
       input.addEventListener(eventName, preventDefaults, false);
     });
   });
 
   //события разукрашивают область, где картинка
-  ['dragenter', 'dragover'].forEach((eventName) => {
+  ["dragenter", "dragover"].forEach((eventName) => {
     fileInputs.forEach((input) => {
       input.addEventListener(eventName, () => highlight(input), false);
     });
   });
   //убирается цвет
-  ['dragleave', 'drop'].forEach((eventName) => {
+  ["dragleave", "drop"].forEach((eventName) => {
     fileInputs.forEach((input) => {
       input.addEventListener(eventName, () => unhighlight(input), false);
     });
   });
 
-  fileInputs.forEach((input) => {
-    input.addEventListener('drop', (e) => {
+  fileInputs.forEach((input: HTMLInputElement) => {
+    input.addEventListener("drop", (e) => {
       //чтоб аватарка уже устанавливалась в профиле когда устанавливаешь новую
       //берём файлы, которые в drag&drop и перетаскиваем в input
-      input.files = e.dataTransfer.files;
+      input.files = (e.dataTransfer as DataTransfer).files;
       let dots;
-      const arr = input.files[0].name.split('.');
+      const arr = input.files[0].name.split(".");
 
-      arr[0].length > 6 ? (dots = '...') : (dots = '.');
+      arr[0].length > 6 ? (dots = "...") : (dots = ".");
       const name = arr[0].substring(0, 6) + dots + arr[1];
-      input.previousElementSibling.textContent = name;
+      (input.previousElementSibling as HTMLElement).textContent = name;
     });
   });
 };
